f216187e3af4e6b866454d2704ead0e38ee564cb
Jordan Jaramillo <62086742+jordanrjdev@users.noreply.github.com>
2021-10-04 18:51:11 -0500

    fix: remove url vercel

--- proyects.js
+++ proyects.js
@@ -5,7 +5,7 @@ export const proyects = [
     description:
       "Aplicación que te permite buscar información acerca de tus peliculas favoritas. Esta aplicacion consume la api de TheMovieDb y la muestra al usuario donde tienes la cantidad de resultados de tu busqueda. Para el desarrollo de esta aplicación fue tomado muy en cuenta el uso de buenas prácticas de desarrollo y permitiendo que esta sea una aplicación facilmente escalable aplicando flujos de trabajo como redux.",
     repository: "https://github.com/jordanrjdev/pelis",
-    demo: "https://pelis-app.vercel.app/",
+    demo: "https://pelis.nadrojdev.xyz",
     technologies: ["Reactjs", "Redux", "TypeScript", "Axios"],
   },
   {
@@ -22,7 +22,7 @@ export const proyects = [
     description:
       "Este sistema esta siendo desarrollado con el stack de tecnologias MERN para evitar la copia de los alumnos. Este proyecto lo que hace es mostrarte evaluaciones pendientes los cuales son generadas a traves de un admin panel por los maestros y podras elegir que pregunta responder la cual tendras tiempo de 30 segundos para responder. Tambien este sistema detecta cuando alguien se sale de la pestaña actual y te resta 1 punto por cada desonnestidad",
     repository: "https://github.com/jordanrjdev/sistemaevaluacion",
-    demo: "https://sistemaevaluacion.vercel.app/",
+    demo: "https://sistemaevaluacion.nadrojdev.xyz/",
     technologies: [
       "Reactjs",
       "Redux",
@@ -38,7 +38,7 @@ export const proyects = [
     description:
       "Este es un template el cual configuré desde 0 react sin el típico comando de create-react-app esto para entender como funsionaba por dentro esta misma herramienta a este proyecto le apliqué varios plugins como la minificacion de archivos, plugins para usar svg y tambien la minificacion de imagenes. Este mismo lo use para desarrollar aplicaciones como el sistema de evaluacion que tambien esta hecho con webpack.",
     repository: "https://github.com/jordanrjdev/-webpack-react-app",
-    demo: "https://webpack-app.vercel.app/",
+    demo: "https://webpack.nadrojdev.xyz/",
     technologies: ["Reactjs", "Webpack"],
   },
   {
